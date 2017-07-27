21f1e59cb80f0011c83438bc8ac0dcfd05ace834
aqudsi <ahmerqudsi@gmail.com>
2017-07-27 17:48:54 -0400

    final commit(hopefully)

--- game.js
+++ game.js
@@ -4,8 +4,7 @@ var wins=0;
 var losses=0;
 var guesses=10;
 var guessesSoFar = [];
-//couldn't get the document.getElementById to work outside of the function psychicGame
-//so i decided to just use an alert to start the game
+
 
 function psychicGame()
 {
@@ -14,34 +13,37 @@ document.onkeyup = function(event)
 	var randomLetter = alphabet[Math.floor(Math.random() * alphabet.length)];
 	var lowerCase = event.key;
 	var userChoice = lowerCase.toUpperCase();
-	guessesSoFar.push(userChoice);
-	
 
-		if(userChoice === randomLetter)
-		{
-			 wins++;
-			 guesses=10;
-			 guessesSoFar=[];
-		}
-		else if (guesses>1 && userChoice !== randomLetter)
-		{
-			guesses-=1;
 
-		}
-		else
-		{
-			 losses++;
-			 guesses=10;
-			 guessesSoFar=[];
-		}
-		console.log(guessesSoFar);
-	
-	
+	if (alphabet.indexOf(userChoice) === -1 )
+	{
+	alert("Please pick a letter!");
+	}
+
+	else if(userChoice === randomLetter)
+	{
+		 wins++;
+		 guesses=10;
+		 guessesSoFar=[];
+	}
+
+	else if (guesses>1 && userChoice !== randomLetter && alphabet.indexOf(userChoice)!== -1)
+	{
+		guesses-=1;
+		guessesSoFar.push(userChoice);
+
+	}
+	else
+	{
+		 losses++;
+		 guesses=10;
+		 guessesSoFar=[];
+	}
 
 	document.getElementById("message").innerHTML = "Psychic Game!" + "<br>" +
-	 "Guess what letter I'm thinking of!" + "<br>" + "Wins: " + wins + "<br>" + "Losses: " +
-	  losses + "<br>" + "You guessed: " + guessesSoFar + "<br>" + "Guesses remaining: " + guesses;	
-}
+	"Guess what letter I'm thinking of!" + "<br>" + "Wins: " + wins + "<br>" + "Losses: " +
+	losses + "<br>" + "You guessed: " + guessesSoFar + "<br>" + "Guesses remaining: " + guesses;	
+	}
 }
 psychicGame();
 
